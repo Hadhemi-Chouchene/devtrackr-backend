0905d8f3dcae475f07ba01eec51e7d14977dca0e
chouchenehadhemi <bidep27062@wisnick.com>
2026-03-27 00:46:51 +0100

    feat: setup MongoDB connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,14 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { JobsModule } from './jobs/jobs.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UsersModule, JobsModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    JobsModule,
+    MongooseModule.forRoot('mongodb://localhost/devtrackr'),
+  ],
 })
 export class AppModule {}
