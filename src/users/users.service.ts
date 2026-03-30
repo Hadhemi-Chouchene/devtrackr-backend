f8d335c06720b7cab9ffb163078be00e4559adaf
chouchenehadhemi <bidep27062@wisnick.com>
2026-03-30 01:34:31 +0100

    feat: implement UsersService with basic methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from './schemas/user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
+
+  async create(email: string, hashedPassword: string): Promise<User> {
+    const createdUser = new this.userModel({ email, password: hashedPassword });
+    return createdUser.save();
+  }
+  async findByEmail(email: string): Promise<User | null> {
+    return this.userModel.findOne({ email }).exec();
+  }
+}
