07e3f2b8f9005320718ca21f0d2662674dec0966
chouchenehadhemi <bidep27062@wisnick.com>
2026-04-01 16:12:54 +0100

    feat: implement user registration with password hashing

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
-
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { UsersService } from 'src/users/users.service';
+import { RegisterDTO } from './dto/register.dto';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async register(registerDTO: RegisterDTO) {
+    const { email, password } = registerDTO;
+
+    // check if user exists
+    const userExists = await this.usersService.findByEmail(email);
+    if (userExists) {
+      throw new BadRequestException('User already exists');
+    }
+    //Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // create user
+    await this.usersService.create(email, hashedPassword);
+
+    return { message: 'User registered successfully' };
+  }
+}
