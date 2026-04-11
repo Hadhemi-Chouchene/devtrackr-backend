25c0160f3a860c8ee7565dd14cc1daa7eab8f0b6
chouchenehadhemi <bidep27062@wisnick.com>
2026-04-12 00:38:34 +0100

    feat: implement login logic with JWT generation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDTO } from './dto/register.dto';
+import { LoginDTO } from './dto/login.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async register(registerDTO: RegisterDTO) {
     const { email, password } = registerDTO;
@@ -20,5 +25,22 @@ export class AuthService {
     await this.usersService.create(email, hashedPassword);
 
     return { message: 'User registered successfully' };
+  }
+  async login(loginDTO: LoginDTO) {
+    const { email, password } = loginDTO;
+    // check if user exists
+    const user = await this.usersService.findByEmail(email);
+    if (!user) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    // check password
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      throw new BadRequestException('Invalid credentials');
+    }
+    // Generate JWT
+    const payload = { sub: user._id.toString(), email: user.email };
+    const token = this.jwtService.sign(payload);
+    return { access_token: token };
   }
 }
