70e7fed1f7bfc01d616e213b01c040bd9adf435f
chouchenehadhemi <bidep27062@wisnick.com>
2026-04-12 02:46:08 +0100

    feat: protect Jobs routes using JwtAuthGuard

--- src/jobs/jobs.controller.ts
+++ src/jobs/jobs.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { JobsService } from './jobs.service';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 
 @Controller('jobs')
-export class JobsController {}
+export class JobsController {
+  constructor(private jobsService: JobsService) {}
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  getJobs() {
+    return { message: 'GET ALL JOBS' };
+  }
+}
