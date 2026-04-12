7caa7d963c620bdd405cd7b9e7d64bfaf83a5eb7
chouchenehadhemi <bidep27062@wisnick.com>
2026-04-12 01:00:28 +0100

    feat: add login endpoint in AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDTO } from './dto/register.dto';
+import { LoginDTO } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   register(@Body() registerDTO: RegisterDTO) {
     return this.authService.register(registerDTO);
+  }
+
+  @Post('login')
+  login(@Body() loginDTO: LoginDTO) {
+    return this.authService.login(loginDTO);
   }
 }
