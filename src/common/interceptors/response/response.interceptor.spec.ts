5aabd6344600a59f7f8e8fdc923418a404061cbd
chouchenehadhemi <bidep27062@wisnick.com>
2026-04-21 14:25:25 +0100

    feat: add global response interceptor for consistent API responses

---
+++ src/common/interceptors/response/response.interceptor.spec.ts
@@ -0,0 +1,7 @@
+import { Common\interceptors\responseInterceptor } from './common\interceptors\response.interceptor';
+
+describe('Common\interceptors\responseInterceptor', () => {
+  it('should be defined', () => {
+    expect(new Common\interceptors\responseInterceptor()).toBeDefined();
+  });
+});
