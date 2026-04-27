1c465935b7fbdc37874cbb92130b7c4af7d51076
chouchenehadhemi <bidep27062@wisnick.com>
2026-04-27 11:05:16 +0100

    feat: implement environment-based configuration and secure JWT setup using ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,29 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { JobsModule } from './jobs/jobs.module';
 import { MongooseModule } from '@nestjs/mongoose';
-
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => {
+        const uri = configService.get<string>('MONGO_URI');
+
+        if (!uri) {
+          throw new Error('MONGO_URI is not defined');
+        }
+
+        return { uri };
+      },
+    }),
     AuthModule,
     UsersModule,
     JobsModule,
-    MongooseModule.forRoot('mongodb://localhost/devtrackr'),
   ],
 })
 export class AppModule {}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
+import { ConfigService } from '@nestjs/config';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { JwtPayload } from './types/jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private configService: ConfigService) {
+    const secret = configService.get<string>('JWT_SECRET');
+
+    if (!secret) {
+      throw new Error('JWT_SECRET is not defined');
+    }
+
     super({
-      // Extract JWT from Authorization: Bearer <token>
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // Secret used to sign and verify JWT
-      secretOrKey: 'secretKey2024_DevTrackr',
+      secretOrKey: secret,
     });
   }
   validate(payload: JwtPayload) {
