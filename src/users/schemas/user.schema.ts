74cc625ae712fa44b9a006017fa3f88a9191fe3f
chouchenehadhemi <bidep27062@wisnick.com>
2026-05-01 13:09:56 +0100

    feat(users): add Role enum and integrate it into user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,10 +1,17 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
+import { Role } from 'src/common/enums/role.enum';
 
 export type UserDocument = User & Document;
 
 @Schema({ timestamps: true })
 export class User {
+  @Prop({
+    type: String,
+    enum: Role,
+    default: Role.USER,
+  })
+  role!: Role;
   @Prop({ required: true, unique: true })
   email!: string;
 
