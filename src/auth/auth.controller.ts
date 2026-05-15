82a8bfcc37bbd552f19dc8e709c074089853795c
chouchenehadhemi <bidep27062@wisnick.com>
2026-05-15 01:14:13 +0100

    feat(auth): protect logout endpoint with JwtAuthGuard

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { CurrentUser } from 'src/common/decorators/current-user.decorator';
 import type { AuthenticatedUser } from './types/authenticated-user.interface';
+import { JwtAuthGuard } from './jwt-auth.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -24,6 +25,7 @@ export class AuthController {
     return this.authService.refreshUserSession(refreshToken);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('logout')
   logout(@CurrentUser() user: AuthenticatedUser) {
     return this.authService.logout(user.userId);
